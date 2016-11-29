caa2971278b00b01d8345e4eb97eb97eb590def9
Billingham <laurence@bgs.ac.uk>
2016-11-29 20:17:49 +0000

    now with default argument for grav accel

--- drop.ex
+++ drop.ex
@@ -1,8 +1,7 @@
 defmodule Drop do
     import :math, only: [sqrt: 1]
-    @accelleration 9.81
-    def fall_velocity(distance)do
-        sqrt(2 * @accelleration * distance)
+    def fall_velocity(distance, gravity \\ 9.81)do
+        sqrt(2 * gravity * distance)
     end
 end
     
