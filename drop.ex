177641fb433e325573856bc48c48d2d378a8f3fa
Billingham <laurence@bgs.ac.uk>
2016-12-03 10:32:53 +0000

    examples of documentation

--- drop.ex
+++ drop.ex
@@ -1,4 +1,26 @@
 defmodule Drop do
+    @moduledoc """
+    Functions for caclulating free-fall in a vaccuum and in constant gravity
+    """
+    @doc """
+    the gravity-parallel speed of a dropped object in a vaccuum.
+    Parameters
+    ----------
+    `distance`:
+        the 'drop height' in meters
+    `gravity`:
+        the gravitational accelertion in ms^1
+    Returns
+    -------
+    `speed`:
+        the velocity (parallel to the gravity vector) of the object
+    after falling `distance`
+    
+     
+    assumes gravitiational acceleration is constant over the
+    region of interest
+    """
+    @spec fall_velocity(number()) :: number()
     import :math, only: [sqrt: 1]
     def fall_velocity(distance, gravity \\ 9.81)do
         sqrt(2 * gravity * distance)
