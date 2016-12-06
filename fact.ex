c8acecace8557e4c450e30724d363fd97e102b21
Billingham <laurence@bgs.ac.uk>
2016-12-06 21:29:47 +0000

    tail-recursive factorial

--- fact.ex
+++ fact.ex
@@ -1,13 +1,14 @@
 defmodule Fact do
-    def factorial(n) when n > 1 do
-        IO.puts("calling from #{n}")
-        result = n * factorial(n-1)
-        IO.puts("#{n} yields #{result}")
-        result
+    def factorial(n) do
+        factorial(1, n, 1)
     end
-    def factorial(n) when n <= 1 do
-        IO.puts("calling from 1")
-        IO.puts("1 yields 1}")
-        1
+    def factorial(current, n, result) when current <= n do
+        new_result = result * current
+        IO.puts("#{current} yields #{new_result}")
+        factorial(current+1, n, new_result)
+    end
+    def factorial(_current, _n, result) do
+        IO.puts("finished")
+        result
     end
 end
