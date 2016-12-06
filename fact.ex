16737c516d9f9322c5ec0c7ba989d9e337d09018
Billingham <laurence@bgs.ac.uk>
2016-12-06 21:30:31 +0000

    factorial/3 should have been private

--- fact.ex
+++ fact.ex
@@ -2,12 +2,12 @@ defmodule Fact do
     def factorial(n) do
         factorial(1, n, 1)
     end
-    def factorial(current, n, result) when current <= n do
+    defp factorial(current, n, result) when current <= n do
         new_result = result * current
         IO.puts("#{current} yields #{new_result}")
         factorial(current+1, n, new_result)
     end
-    def factorial(_current, _n, result) do
+    defp factorial(_current, _n, result) do
         IO.puts("finished")
         result
     end
